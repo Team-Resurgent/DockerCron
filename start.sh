545fe7170d72cef160c5b2f8f3ac4391fbe2db72
EqUiNoX <87106037+EqUiNoX-Labs@users.noreply.github.com>
2023-07-05 11:42:12 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [ -z "$SCHEDULER_ENVIRONMENT" ]; then
-   echo "SCHEDULER_ENVIRONMENT not set, assuming Development"
-   SCHEDULER_ENVIRONMENT="Development"
-fi
-
 # Select the crontab file based on the environment
-CRON_FILE="crontab.$SCHEDULER_ENVIRONMENT"
+CRON_FILE="crontab.sh"
 
 echo "Loading crontab file: $CRON_FILE"
 
